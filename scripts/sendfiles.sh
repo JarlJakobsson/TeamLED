b1a6e7e52a960957e19d4b9df1ec6b9b69442c4b
JustOwl <ka03bj@gmail.com>
2023-09-07 09:44:57 +0200

    Added comments

--- scripts/sendfiles.sh
+++ scripts/sendfiles.sh
@@ -2,5 +2,7 @@
 
 for file in ./*.gz; do
     # Fill in correct SSH info, keys should allow us to skip any passwords
-    scp $file username@ip-address:/destination/folder
+    # To get the keys (might not be best practice)
+    # Run command ssh-copy-id user@ip-adress
+    scp $file user@ip-address:/destination/folder
 done
